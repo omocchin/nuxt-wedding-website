db02139e4c600c9f731a0e7701acaddd28cf392f
Kyle Mitsui <omocchin@gmail.com>
2024-08-23 00:52:25 +0900

    configure theme color

---
+++ plugins/vuetify.ts
@@ -0,0 +1,25 @@
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+import { createVuetify, type ThemeDefinition } from 'vuetify'
+
+const primaryTheme: ThemeDefinition = {
+  dark: false,
+  colors: {
+    background: 'D8D9CF',
+    primary: 'EDEDED',
+    secondary: 'FF8787',
+    accent: 'E26868'
+  },
+}
+
+export default defineNuxtPlugin((app) => {
+  const vuetify = createVuetify({
+    theme: {
+      defaultTheme: 'primaryTheme',
+      themes: {
+        primaryTheme
+      },
+    },
+  })
+  app.vueApp.use(vuetify)
+})
